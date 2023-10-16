78a181641d6ad8c33261e4a55c5a907c9ddec659
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2023-10-16 16:46:43 +0500

    higher order component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { Button, Card, Heading, Input, Rating, Tag, Text, TextArea } from '../components';
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
+import { withLayout } from '@/layout/layout';
 
 const Index = () => {
 	const [isClick, setIsClick] = useState(false);
 	const [rating, setRating] = useState<number>(4);
 	return (
-		<div>
+		<>
 			<Heading tag='h2'>Heading</Heading>
 			<Text size='s'>Text</Text>
 			<Tag size='s' color='red'>
@@ -44,8 +47,18 @@ const Index = () => {
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sunt delectus ipsum accusantium. Quaerat necessitatibus
 				laborum cum quis veniam. Eveniet?
 			</Card>
-		</div>
+		</>
 	);
 };
 
-export default Index;
+export default withLayout(Index);
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const { data } = await axios.post('http://localhost:8100/page-find', { firstCategory: 1 });
+
+	return {
+		props: {
+			data,
+		},
+	};
+};
